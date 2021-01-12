728c98f9f1653f7c1fc0807703a47c43af3ef161
iaido <lad1@naver.com>
2021-01-12 15:59:48 +0900

    config 변경

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -3,7 +3,6 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flaskext.markdown import Markdown
-import config
 
 naming_convention = {
     "ix": 'ix_%(column_0_label)s',
@@ -17,7 +16,7 @@ migrate = Migrate()
 
 def create_app() :
     app = Flask(__name__)
-    app.config.from_object(config)
+    app.config.from_envvar('APP_CONFIG_FILE')
 
     db.init_app(app)
     if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
