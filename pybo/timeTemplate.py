51a46993741927cdf89351d93f1519ed8397359b
iaido <lad1@naver.com>
2021-01-12 11:20:05 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/timeTemplate.py
@@ -0,0 +1,2 @@
+def format_datetime(value, fmt='%Y년 %m월 %d일 %H시%M분'):
+    return value.strftime(fmt)
---
+++ pybo/views/main_views.py
@@ -0,0 +1,23 @@
+##from flask import Blueprint, render_template
+from flask import Blueprint, url_for
+##from pybo.models import Question
+from werkzeug.utils import redirect
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+@bp.route('/')
+def index():
+    # return 'Pybo index!'
+    ##question_list = Question.query.order_by(Question.create_date.desc())
+    ##return render_template('question/question_list.html', question_list=question_list)
+    return redirect(url_for('question._list'))
+##@bp.route('/detail/<int:question_id>/')
+##def detail(question_id):
+    #question = Question.query.get(question_id)
+    ##  question = Question.query.get_or_404(question_id)
+    ##return render_template('question/question_detail.html', question=question)
